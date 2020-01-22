c82e3a8b635eb027f54258c3899b97c47d1a5a2b
sandeep-negi-03 <sandeepnegi03@gmail.com>
2020-01-22 16:24:35 +0530

    changes for amdocs test classes

--- src/com/ad/test/Test4.java
+++ src/com/ad/test/Test4.java
@@ -1,42 +1,86 @@
 package com.ad.test;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Test4 {
 
-	public int deserialize(byte[] stream){
-		
-		
-		
-		return 0;
+	public int deserialize(byte[] stream) throws IOException, ClassNotFoundException {
+		int sum = 0;
+		List<Person> personList = new ArrayList<Person>();
+
+		ByteArrayInputStream bis = new ByteArrayInputStream(stream);
+		ObjectInputStream in = new ObjectInputStream(bis);
+		personList = (ArrayList<Person>) in.readObject();
+
+		for (Person person : personList) {
+			System.out.println(person.getAge());
+			sum += person.getAge();
+		}
+
+		return sum;
 	}
-	
-	
-	
-	class Person implements Serializable{
-		
-		private String name;
-		private int age;
-		private int id;
-		public String getName() {
-			return name;
-		}
-		public void setName(String name) {
-			this.name = name;
-		}
-		public int getAge() {
-			return age;
-		}
-		public void setAge(int age) {
-			this.age = age;
-		}
-		public int getId() {
-			return id;
-		}
-		public void setId(int id) {
-			this.id = id;
-		}
-		
-		
+
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
+
+		List<Person> personList = new ArrayList<Person>();
+		personList.add(new Person("A", 10, 101));
+		personList.add(new Person("B", 20, 201));
+
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		ObjectOutputStream oos = new ObjectOutputStream(bos);
+
+		oos.writeObject(personList);
+
+		byte[] stream = bos.toByteArray();
+
+		Test4 test4 = new Test4();
+		System.out.println("Total age : " + test4.deserialize(stream));
 	}
+
+}
+
+class Person implements Serializable {
+
+	private String name;
+	private int age;
+	private int id;
+
+	public Person(String name, int age, int id) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
 }
